2d11cf986a7c123a4db6464f14bbabb9d8babab8
stharrold <samuel.harrold@gmail.com>
2013-10-10 01:27:26 +0000

    marketsim.py reads/writes

--- Homework_3/marketsim.py
+++ Homework_3/marketsim.py
@@ -7,12 +7,26 @@ Usage:
 $ python marketsim.py 1000000 orders.csv values.csv
 """
 
+import matplotlib
+matplotlib.use('Agg')
+
 import sys
+import pandas as pd
+import numpy as np
 
 def main():
-    list = sys.argv[1:]
-    print list
+    # test that only 3 arguments
+    # test that csv file exists
+    # test that not overwriting another csv file
+    [start_value, orders_file, values_file] = sys.argv[1:4]
+    # loading text from: http://wiki.quantsoftware.org/index.php?title=QSTK_Tutorial_2
+    orders = pd.read_csv(orders_file, header=False, names=['year', 'month', 'day', 'symbol', 'action', 'shares'],\
+                             dtype={'year':int, 'month':int, 'day':int, 'symbol':str, 'action':str, 'shares':int})
+    orders.to_csv(values_file, header=False, index=False, mode='a')
+    # with open(values_file, "a") as vf:
+    #     vf.write(start_value)
+    #     vf.write("\n")
     return
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
