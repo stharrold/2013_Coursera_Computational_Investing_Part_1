dfd1b24e1740ea38fc6f48426b2721a325835400
stharrold <samuel.harrold@gmail.com>
2013-10-10 00:10:13 +0000

    Added marketsim.py and orders.csv

---
+++ Homework_3/marketsim.py
@@ -0,0 +1,18 @@
+#! /bin/env python
+"""
+Homework 3
+http://wiki.quantsoftware.org/index.php?title=CompInvesti_Homework_3
+Market simulation
+Usage:
+$ python marketsim.py 1000000 orders.csv values.csv
+"""
+
+import sys
+
+def main():
+    list = sys.argv[1:]
+    print list
+    return
+
+if __name__ = '__main__':
+    main()
