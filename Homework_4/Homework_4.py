965b3d1f144398ea34959eb4e3fcab638017aeaa
stharrold <samuel.harrold@gmail.com>
2013-10-14 02:40:10 +0000

    Moved student solutions to current directory.

--- Homework_4/Homework_4.py
+++ Homework_4/Homework_4.py
@@ -4,6 +4,6 @@ Homework_4.py
 """
 
 import sys
-sys.path.append('~/2013_Coursera_Computational_Investing_Part_1/Solutions/Homework_3')
+sys.path.append("~/2013_Coursera_Computational_Investing_Part_1/Solutions/Homework_3")
 import marketsim_from_student
 import analyze_from_student
