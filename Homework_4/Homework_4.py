2f3a03d023ef13ae847c8ddb7f093697602aa597
stharrold <samuel.harrold@gmail.com>
2013-10-15 22:32:23 +0000

    Redid Homework_4.py again from TA-posted solutions to Homework 2.

--- Homework_4/Homework_4.py
+++ Homework_4/Homework_4.py
@@ -1,250 +1,156 @@
 #! /usr/bin/env python
 """
 Homework_4.py
+Find events, create orders, calculate fund value.
 """
 
 import matplotlib
 matplotlib.use('Agg')
 
 import QSTK.qstkutil.qsdateutil as du
-import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkutil.DataAccess as da
+import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkstudy.EventProfiler as ep
 
 import sys
+import csv
 import time
 import math
-import pandas as pd
-import numpy as np
-import datetime as dt
 import copy
-
-def find_events(ls_symbols, df_close, f_event_threshold):
+import numpy as np
+import pandas as pd
+import datetime as dt
+ 
+def create_df_events(ls_symbols, df_close, f_cutoff):
     """
-    Accepts a list of symbols and stock data frame.
-    Returns pandas dataframe of events:
-        |IBM |GOOG|XOM |MSFT| GS | JP |
-    (d1)|nan |nan |1   |nan |nan |1   |
-    (d2)|nan |1   |nan |nan |nan |nan |
-    (d3)|1   |nan |1   |nan |1   |nan |
-    (d4)|nan |1   |nan |1   |nan |nan |
+    Create events dataframe.
+    Accepts a list of symbols along with start and end date
+    Returns the Event Matrix which is a pandas DataFrame
+    Event matrix has the following structure :
+    |IBM |GOOG|XOM |MSFT| GS | JP |
+    (d1)|nan |nan |True|nan |nan |True|
+    (d2)|nan |True|nan |nan |nan |nan |
+    (d3)|True|nan |True|nan |True|nan |
+    (d4)|nan |True|nan |True|nan |nan |
     ...................................
     ...................................
     Also, d1 = start date
-    nan = no information about any event.
-    1 = event occured.
+    False negatively confirms the event occurance
+    True positively confirms the event occurence
     """
-
-    # Finding the event dataframe 
-    # ts_market = df_close['SPY']
-    # Creating an empty dataframe
-    df_events = copy.deepcopy(df_close)
-    df_events = df_events * np.NAN
-    # Time stamps for the event range
     ldt_timestamps = df_close.index
+    df_events = pd.DataFrame(index=ldt_timestamps, columns=ls_symbols)
+    # TODO: Vectorize using arith on series
     for s_sym in ls_symbols:
-        for i in range(1, len(ldt_timestamps)):
-            # Calculating the returns for this timestamp
-            f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
-            f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i-1]]
-            # f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
-            # f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
-            # f_symreturn_today = (f_symprice_today / f_symprice_yest) - 1
-            # f_marketreturn_today = (f_marketprice_today / f_marketprice_yest) - 1
-            # Event is found if actual close of the stock price drops below $5
-            if f_symprice_today < f_event_threshold and f_symprice_yest >= f_event_threshold:
-                df_events[s_sym].ix[ldt_timestamps[i]] = 1
+        # TODO: Vectorize using arith on subframes
+        for dt_timestamp in ldt_timestamps:
+            if dt_timestamp == ldt_timestamps[0]:
+                dt_yesterday = dt_timestamp
+                f_sym_price_yesterday = df_close[s_sym].ix[dt_yesterday]
+                continue
+            dt_today = dt_timestamp
+            f_sym_price_today = df_close[s_sym].ix[dt_today]
+             # TODO: vectorize using where on temp frame
+            if f_sym_price_yest >= f_cutoff and f_sym_price_today < f_cutoff:
+                df_events[s_sym].ix[dt_today] = True
+            else:
+                False
+            dt_yesterday = dt_today
+            f_sym_price_yesterday = f_sym_price_today
     return df_events
 
-def create_orders(ls_symbols, df_events):
+def create_df_orders(ls_symbols, df_events):
     """
     Create market orders from events.
+    Returns a dataframe.
     """
-
-    # TODO: Take symbols from events.
-    # TODO: Don't loop. Use where.
-    ls_names = ['timestamp', 'symbol', 'action', 'shares']
-    df_orders = pd.DataFrame(columns=ls_names)
+    # TODO: Could skip creating this order list and save time.
     ldt_timestamps = df_events.index
-    for sym in ls_symbols:
-        for its, ts_today in enumerate(ldt_timestamps):
-            if df_events[sym].ix[ts_today] == 1:
-                s_new_buy_order = pd.DataFrame([{'timestamp':ts_today, 'symbol':sym, 'action':'buy', 'shares':100}])
-                df_orders = df_orders.append(s_new_buy_order, ignore_index=True)
+    ls_columns = ['timestamp', 'symbol', 'action', 'shares']
+    df_orders = pd.Dataframe(columns=ls_columns)
+    # TODO: Vectorize using arith on subframess
+    for i_idx, dt_timestamp in enumerate(ldt_timestamps):
+        # TODO: Take symbols from events
+        # TODO: Vectorize using arith on series
+        for s_sym in ls_symbols:
+            # TODO: Use where instead of looping.
+            if df_events[sym].ix[dt_timestamp] == True:
+                dt_buy_date = dt_timestamp
+                df_buy_order = pd.DataFrame([{'timestamp':dt_buy_date,
+                                              'symbol':s_sym,
+                                              'action':'buy'
+                                              'shares':100.}])
+                # TODO: Don't add rows to a dataframe, inefficient.
+                df_orders = df_orders.append(df_buy_order, ignore_index=True, verify_integrity=True)
                 try:
-                    ts_todayp5 = ldt_timestamps[its+5]
-                    s_new_sell_order = pd.DataFrame([{'timestamp':ts_todayp5, 'symbol':sym, 'action':'sell', 'shares':100}])
-                    df_orders = df_orders.append(s_new_sell_order, ignore_index=True)
+                    dt_sell_date = ldt_timestamps[i_idx+5]
+                    df_sell_order = pd.DataFrame([{'timestamp':dt_sell_date,
+                                                  'symbol':s_sym,
+                                                  'action':'sell'
+                                                  'shares':100.}])
+                    df_orders = df_orders.append(df_sell_order, ignore_index=True, verify_integrity=True)
                 except:
-                    print "ldt_timestamps[its+5] not possible for ", ts_today
+                    print "ldt_timestamps[i_idx+5] not possible for ", dt_buy_date
     df_orders.sort(columns='timestamp', inplace=True)
+    df_orders.index=range(len(df_test))[::-1]
     return df_orders
 
-def calculate_positions(f_start_cash, ls_symbols, df_orders, df_close):
-    # Create data frame for positions.
+def create_df_positions(f_start_cash, ls_symbols, df_orders, df_close):
+    """
+    Create a data frame of positions in cash and shares.
+    """
     ldt_timestamps = df_close.index
+    # TODO: Get symbols from df_close.
     ls_columns = ['cash'] + ls_symbols
     df_positions = pd.DataFrame(0., index=ldt_timestamps, columns=ls_columns)
-    # Update positions one day at a time.
-    # TODO: don't use loops
-    for its, ts_today in enumerate(ldt_timestamps):
-        if its == 0:
-            df_positions['cash'].ix[ts_today] = f_start_cash
+    # TODO: Vectorize using arith on series.
+    for dt_timestamp in ldt_timestamps:
+        dt_today = dt_timestamp
+        # TODO: Use where on temp data fraem.
+        if dt_today == ldt_timestamps[0]:
+            df_positions['cash'].ix[dt_today] = f_start_cash
         else:
-            df_positions.ix[ts_today] = df_positions.ix[ts_yesterday]
-        for iord, s_order in df_orders.iterrows():
-            ts_order = s_order['timestamp']
-            if ts_today == ts_order:
-                act = s_order['action']
-                sym = s_order['symbol']
-                shr = s_order['shares']
-                pri = df_close[sym].ix[ts_today]
-                cash = df_positions['cash'].ix[ts_today]
-                tot_shr = df_positions[sym].ix[ts_today]
-                # TODO: positions.ix[ts_today, [sym]] causes TypeError. Why?
-                if act == 'buy':
-                    df_positions['cash'].ix[ts_today] = np.subtract(cash, np.dot(shr, pri))
-                    df_positions[sym].ix[ts_today]    = np.add(tot_shr, shr)
-                elif act == 'sell':
-                    df_positions['cash'].ix[ts_today] = np.add(cash, np.dot(shr, pri))
-                    df_positions[sym].ix[ts_today]    = np.subtract(tot_shr, shr)
-                else:
-                    print "ERROR: act neither buy nor sell."
-                    print "act = ", act
-        ts_yesterday = ts_today
+            df_positions.ix[dt_today] = df_positions.ix[dt_yesterday]
+            for df_order in df_orders.iterrows():
+                if dt_today == df_order['timestamp']:
+                    s_act = df_order['action']
+                    s_sym = df_order['symbol']
+                    f_shr = df_order['shares']
+                    f_pri = df_close[sym].ix[dt_today]
+                    if s_act == 'buy':
+                        df_positions['cash'].ix[dt_today] -= f_shr * f_pri
+                        df_positions[sym].ix[dt_today]    += f_shr
+                    elif s_act == 'sell':
+                        df_positions['cash'].ix[dt_today] += f_shr * f_pri
+                        df_positions[sym].ix[dt_today]    -= f_shr
+                    else:
+                        print "ERROR: s_act neither buy nor sell."
+                        print "s_act = ", s_act
+        dt_yesterday = dt_today
     return df_positions
 
-def calculate_values(ls_symbols, df_positions, df_close):
+def create_df_values(ls_symbols, df_positions, df_close):
     """
-    Calculate fund values.
+    Create a dataframe of values from cash and share positions.
     """
-
-    # Write values from positions.
     ldt_timestamps = df_close.index
     df_values = pd.DataFrame(0., index=ldt_timestamps, columns=['value'])
-    for its, ts_today in enumerate(ldt_timestamps):
-        df_values['value'].ix[ts_today] = df_positions['cash'].ix[ts_today]
-        # TODO: positions.ix[ts_today, [sym]] causes TypeError. Why?
-        for sym in ls_symbols:
-            f_stock_value = np.dot(df_positions[sym].ix[ts_today], df_close[sym].ix[ts_today])
-            df_values['value'].ix[ts_today] = np.add(df_values['value'].ix[ts_today], f_stock_value)
+    # TODO: Vectorize arith on subframes
+    for dt_timestamp in ldt_timestamps:
+        dt_today = dt_timestamp
+        df_values['value'].ix[dt_today] = df_positions['cash'].ix[dt_today]
+        # TODO: Get symbols from df_close.
+        # TODO: Vectorize using arith on series.
+        for s_sym in ls_symbols:
+            f_shr = df_positions[sym].ix[dt_today]
+            f_pri = df_close[sym].ix[dt_today]
+            df_values['value'].ix[dt_today] += f_share * f_pri
     return df_values
 
 def main():
     """
-    Main function
+    Main program.
     """
 
-    # Reading the historical data.
-    # dt_start = df_orders.index.min()
-    print "dt_start = ", dt_start
-    # dt_end = df_orders.index.max()
-    print "dt_end = ", dt_end
-    # We need closing prices so the timestamp should be hours=16.
-    dt_timeofday = dt.timedelta(hours=16)
-    # Get a list of trading days between the start and the end.
-    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
-
-    # Import data.
-    print "Import data."
-    # Create an object of DataAccess class with Yahoo as the source.
-    c_dataobj = da.DataAccess('Yahoo')
-    # Get a list of symbols we are going to trade
-    list_name = 'sp5002012'
-    ls_symbols = c_dataobj.get_symbols_from_list(list_name)
-    ls_symbols.append('SPY')
-    # ls_all_symbols = c_dataobj.get_all_symbols()
-    # # Bad symbols are symbols present in portfolio but not in all symbols
-    # ls_bad_symbols = list(set(ls_symbols) - set(ls_all_symbols))
-    # if len(ls_bad_symbols) != 0:
-    #     print "Orders contain bad symbols : ", ls_bad_symbols
-    #     return
-    # Keys to be read from the data, it is good to read everything in one go.
-    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-    # Reading the data, now d_data is a dictionary with the keys above.
-    # Timestamps and symbols are the ones that were specified before.
-    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
-    d_data = dict(zip(ls_keys, ldf_data))
-    # Copying the close price into separate DataFrame
-    df_close = d_data['close'].copy()
-    # Filling the gaps in data.
-    for s_symbol in ls_symbols:
-        df_close[s_symbol] = df_close[s_symbol].fillna(method='ffill')
-        df_close[s_symbol] = df_close[s_symbol].fillna(method='bfill')
-        df_close[s_symbol] = df_close[s_symbol].fillna(1.0)
-
-    # Create events data frame.
-    print "Finding events."
-    df_events = find_events(ls_symbols, df_close, f_event_threshold)
-    print "Writing events.csv."
-    df_events.to_csv('events.csv')
-
-    # Create orders data frame.
-    print "Creating orders."
-    df_orders = create_orders(ls_symbols, df_events)
-    print "Writing orders.csv."
-    df_orders.to_csv('orders.csv')
-
-    # Calculate positions.
-    print "Calculating positions."
-    df_positions = calculate_positions(f_start_cash, ls_symbols, df_orders, df_close)
-    print "Writing positions.csv."
-    df_positions.to_csv('positions.csv')
-
-    # Calculate fund value.
-    print "Calculating fund values."
-    df_values = calculate_values(ls_symbols, df_positions, df_close)
-    print "Writing values.csv."
-    values.to_csv('values.csv')
-
-    # for fund
-    na_price_fund = df_values['value'].values * 1.0
-    na_normalized_price_fund = na_price_fund / na_price_fund[0, :]
-    na_returns_fund = na_normalized_price_fund.copy()
-    tsu.returnize0(na_returns_fund)
-    daily_return_fund = na_returns_fund
-    avg_daily_return_fund = np.average(daily_return_fund)
-    std_daily_return_fund = np.std(daily_return_fund)
-    # sharpe_ratio_fund = tsu.get_sharpe_ratio(rets=daily_ret_fund, risk_free=0.00)
-    sharpe_ratio_fund = math.sqrt(252.0) * avg_daily_return_fund / std_daily_return_fund
-    total_return_fund = na_normalized_price_fund[-1]
-    
-    # for comparision symbol
-    # TODO: Flatten na_price list of lists.
-    na_price_comp = df_close['SPY'].values * 1.0
-    na_normalized_price_comp = na_price_comp / na_price_comp[0]
-    na_returns_comp = na_normalized_price_comp.copy()
-    tsu.returnize0(na_returns_comp)
-    daily_return_comp = na_returns_comp
-    avg_daily_return_comp = np.average(daily_return_comp)
-    std_daily_return_comp = np.std(daily_return_comp)
-    # sharpe_ratio_comp = tsu.get_sharpe_ratio(rets=daily_return_comp, risk_free=0.00)
-    sharpe_ratio_comp = math.sqrt(252.0) * avg_daily_return_comp / std_daily_return_comp
-    total_return_comp = na_normalized_price_comp[-1]
-
-    print "sharpe_ratio_fund = ", sharpe_ratio_fund
-    print "sharpe_ratio_comp = ", sharpe_ratio_comp
-
-    print "total_return_fund = ", total_return_fund
-    print "total_return_comp = ", total_return_comp
-
-    print "std_daily_return_fund = ", std_daily_return_fund
-    print "std_daily_return_comp = ", std_daily_return_comp
-
-    print "avg_daily_return_fund = ", avg_daily_return_fund
-    print "avg_daily_return_comp = ", avg_daily_return_comp
-
-    return
-
-
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        f_start_cash = 50000.0
-        # s_orders_filename = 'orders.csv'
-        # s_values_filename = 'values.csv'
-        f_event_threshold = 5.0
-        dt_start = dt.datetime(2008, 01, 01, 16)
-        dt_end   = dt.datetime(2009, 12, 31, 16)
-        main()
-    else:
-        print "Usage: ./Homework_4.py"
+    main()
--- Homework_4/analyze.py
+++ Homework_4/analyze.py
@@ -1,175 +1,64 @@
-"""
-Author     : Tolunay Orkun <tolunay(at)orkun(dot)us>
-Date       : Oct 7, 2013
-Description: Fund Analyzer for Computational Investing HW3
-"""
-import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.tsutil as tsu
 import QSTK.qstkutil.DataAccess as da
-
 import datetime as dt
 import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
-import math
-import time
+from pylab import *
 import sys
+import pandas
+import csv
 
 
-def read_values(valuesfile):
-    """Read values to a Panda DataFrame"""
-
-    li_cols = [0, 1, 2, 3]
-    ls_names = ['YEAR', 'MONTH', 'DAY', 'TOTAL']
-    d_date_columns = { 'DATE': ['YEAR', 'MONTH', 'DAY']}
-    s_index_column = 'DATE'
-    df_values = pd.read_csv(valuesfile, \
-                            dtype={'TOTAL': np.float64}, \
-                            sep=',', \
-                            comment='#', \
-                            skipinitialspace=True, \
-                            header=None, \
-                            usecols=li_cols, \
-                            names=ls_names, \
-                            parse_dates=d_date_columns, \
-                            index_col=s_index_column)
-    if not df_values.index.is_monotonic:
-        df_values.sort_index(inplace=True)
-
-    return df_values
+def _csv_read_fund(filename):
+    reader = csv.reader(open(filename, 'rU'), delimiter=',')
+    vals = []
+    dates = []
+    for row in reader:
+        vals.append(float(row[3]))
+        date = dt.datetime(int(row[0]), int(row[1]), int(row[2]), 16)
+        dates.append(date)
+    ts_fund = pandas.TimeSeries(dict(zip(dates, vals)))
+    return ts_fund
 
 
-def main():
-    """Main Function"""
+def _read_bench(symbol, timestamps):
 
-    print "\nProcessing %s...\n" % s_values_filename
+    dataobj = da.DataAccess('Yahoo')
+    close = dataobj.get_data(timestamps, [symbol], "close", verbose=True)
+    close = close.fillna(method='ffill')
+    close = close.fillna(method='bfill')
+    return close[symbol]
 
-    # Reading the values
-    df_values = read_values(s_values_filename)
+ts_fund = _csv_read_fund(sys.argv[1])
+benchmark = sys.argv[2]
+bench_vals = _read_bench(benchmark, list(ts_fund.index))
+# print bench_vals
+multiple = ts_fund[0] / bench_vals[0]
+bench_vals = bench_vals * multiple
 
-    # Extract just the data (as floating point values)
-    na_values = df_values.values * 1.0
+print "Details of the Performance of the portfolio"
+print 'Data Range : ', ts_fund.index[0], ' to ', ts_fund.index[-1]
+print 'Sharpe Ratio of Fund :', tsu.get_sharpe_ratio(tsu.daily(ts_fund))[0]
+print 'Sharpe Ratio of ' + benchmark + ' :', tsu.get_sharpe_ratio(
+                                          tsu.daily(bench_vals))[0]
+print 'Total Return of Fund : ', (((ts_fund[-1] / ts_fund[0]) - 1) + 1)
+print 'Total Return of ' + benchmark + ' :', (((bench_vals[-1]
+                                            / bench_vals[0]) - 1) + 1)
+print 'Standard Deviation of Fund : ', np.std(tsu.daily(
+                                       ts_fund.values))
+print 'Standard Deviation of ' + benchmark + ' :', np.std(
+                                       tsu.daily(bench_vals.values))
 
-    # Normalize
-    na_norm_values = na_values / na_values[0, :]
+print 'Average Daily Return of Fund : ', np.mean(tsu.daily(
+                                       ts_fund.values))
+print 'Average Daily Return of ' + benchmark + ' :', np.mean(
+                                       tsu.daily(bench_vals.values))
 
-    # Returnize the values
-    na_returns = na_norm_values.copy()
-    tsu.returnize0(na_returns)
-
-    # Calculate statistical values
-    f_avg_return = np.mean(na_returns)
-    f_tot_return = np.prod(na_returns + 1.0)
-    f_stdev = np.std(na_returns)
-    f_sharpe = math.sqrt(252.0) * f_avg_return / f_stdev
-
-    # Reading the historical data.
-    dt_start = df_values.index[0]
-    dt_end = df_values.index[len(df_values.index) - 1]
-
-    # We need closing prices so the timestamp should be hours=16.
-    dt_timeofday = dt.timedelta(hours=16)
-
-    # Get a list of trading days between the start and the end.
-    ldt_timestamps = du.getNYSEdays(dt_start, dt_end + dt.timedelta(days=1), dt_timeofday)
-
-    # Keys to be read from the data, it is good to read everything in one go.
-    ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-
-    # Prepare the symbols to fetch from data source
-    ls_symbols = [ s_benchmark_symbol ]
-
-    # Create an object of DataAccess class with Yahoo as the source.
-    c_dataobj = da.DataAccess('Yahoo')
-
-    # Reading the data, now d_data is a dictionary with the keys above.
-    # Timestamps and symbols are the ones that were specified before.
-    ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
-    d_data = dict(zip(ls_keys, ldf_data))
-
-    # Copying the close price into separate DataFrame
-    df_close = d_data['close'].copy()
-
-    # Filling the gaps in data.
-    df_close[s_benchmark_symbol] = df_close[s_benchmark_symbol].fillna(method='ffill')
-    df_close[s_benchmark_symbol] = df_close[s_benchmark_symbol].fillna(method='bfill')
-    df_close[s_benchmark_symbol] = df_close[s_benchmark_symbol].fillna(1.0)
-
-    # Extract just the data
-    na_benchmark_close = df_close.values
-
-    # Normalize the price
-    na_norm_benchmark_close = na_benchmark_close / na_benchmark_close[0, :]
-
-    # Returnize the values
-    na_benchmark_returns = na_norm_benchmark_close.copy()
-    tsu.returnize0(na_benchmark_returns)
-
-    # Calculate statistical values
-    f_benchmark_avg_return = np.mean(na_benchmark_returns)
-    f_benchmark_tot_return = np.prod(na_benchmark_returns + 1.0)
-    f_benchmark_stdev = np.std(na_benchmark_returns)
-    f_benchmark_sharpe = math.sqrt(252.0) * f_benchmark_avg_return / f_benchmark_stdev
-
-    print 'The final value of the portfolio using the sample file is -- %d,%d,%d,%.0f' % \
-          (dt_end.year, dt_end.month, dt_end.day, df_values['TOTAL'].ix[dt_end])
-    print
-    print 'Details of the Performance of the portfolio'
-    print
-    print 'Date Range : ', min(ldt_timestamps), ' to ', max(ldt_timestamps)
-    print
-    print 'Sharpe Ratio of Fund : %-.12g' % f_sharpe
-    print 'Sharpe Ratio of %s : %-.12g' % (s_benchmark_symbol, f_benchmark_sharpe)
-    print
-    print 'Total Return of Fund :  %-.12g' % f_tot_return
-    print 'Total Return of %s : %-.12g' % (s_benchmark_symbol, f_benchmark_tot_return)
-    print
-    print 'Standard Deviation of Fund :  %-.12g' % f_stdev
-    print 'Standard Deviation of %s : %-.12g' % (s_benchmark_symbol, f_benchmark_stdev)
-    print
-    print 'Average Daily Return of Fund :  %-.12g' % f_avg_return
-    print 'Average Daily Return of %s : %-.12g' % (s_benchmark_symbol, f_benchmark_avg_return)
-
-    # Plotting the prices with x-axis=timestamps
-    plt.clf()
-    fig = plt.figure()
-    fig.add_subplot(111)
-    plt.plot(ldt_timestamps, na_values)
-    plt.plot(ldt_timestamps, na_norm_benchmark_close * na_values[0], alpha=0.4)
-    plt.legend(['Fund', s_benchmark_symbol], loc='best')
-    plt.ylabel('Value')
-    plt.xlabel('Date')
-    fig.autofmt_xdate(rotation=45)
-    s_pdf_filename = s_values_filename[ : s_values_filename.rfind('.')] + '.pdf'
-    plt.savefig(s_pdf_filename, format='pdf')
-
-    # Plot again using normalized values
-    plt.clf()
-    fig = plt.figure()
-    fig.add_subplot(111)
-    plt.plot(ldt_timestamps, na_norm_values)
-    plt.plot(ldt_timestamps, na_norm_benchmark_close, alpha=0.4)
-    plt.legend(['Fund', s_benchmark_symbol], loc='best')
-    plt.ylabel('Normalized Value')
-    plt.xlabel('Date')
-    fig.autofmt_xdate(rotation=45)
-    s_pdf_filename = s_values_filename[ : s_values_filename.rfind('.')] + '_normalized.pdf'
-    plt.savefig(s_pdf_filename, format='pdf')
-
-
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        s_values_filename = 'values.csv'
-        s_benchmark_symbol = '$SPX'
-        main()
-        s_values_filename = 'values2.csv'
-        s_benchmark_symbol = '$SPX'
-        main()
-    elif len(sys.argv) == 3:
-        s_values_filename = sys.argv[1]
-        s_benchmark_symbol = sys.argv[2]
-        main()
-    else:
-        print "\nUsage: python analyze.py <values.csv file> <benchmark>\n"
+plt.clf()
+plt.plot(ts_fund.index, ts_fund.values)
+plt.plot(ts_fund.index, bench_vals)
+plt.ylabel('Fund Value', size='xx-small')
+plt.xlabel('Date', size='xx-small')
+plt.legend(['Fund', 'Benchmark'], loc='best')
+plt.xticks(size='xx-small')
+plt.yticks(size='xx-small')
+savefig('chart.pdf', format='pdf')
